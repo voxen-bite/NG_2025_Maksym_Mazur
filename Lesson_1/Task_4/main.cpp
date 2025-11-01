7981b7d9ff83d411547531ff5b11049cf66b1247
voxen-bite <voxen001@gmail.com>
2025-11-01 22:57:12 +0200

    fix commit error

--- Lesson_1/Task_4/main.cpp
+++ Lesson_1/Task_4/main.cpp
@@ -1,25 +1,48 @@
 #include <iostream>
-#include <cmath>
+
 using namespace std;
 
-int main(){
-    long double num1, num2, result;
-    char op;
-
-    cout <<"Enter youre first number \n"; 
-    cin >> num1;
-    cout <<"Enter youre second number \n";
-    cin >> num2;
-    cout <<"Enter operation: +, -, *, /, ^, sqrt.\n";
-    cin >> op;
-    switch (op)
-    {
-    case (+):
-        result = num1 + num2;
-        cout <<"Sum: " <<result; 
-        break;
-    
-    default:
+int main()
+{
+    char type;
+    int num1;
+    cout <<"Which figure do you wanna draw? s / t" <<endl;
+    cin >> type;
+    switch (type){
+        case 's': //square
+            cout <<"What size do you wanna draw?" <<endl;
+            cin >> num1;
+            if (num1 > 0){
+                for(int i = 0; i < num1; i++){
+                    for(int j = 0; j < num1; j++){
+                        cout <<"*";
+                    }
+                    cout <<endl;
+                }
+                cout <<"[-] Done" <<endl;
+            }else{
+                cout <<"Error: size can't be <= 0" <<endl;
+                return 0;
+            }
+            break;
+        case 't': // triangle
+            cout <<"What size do you wanna draw?" <<endl;
+            cin >> num1;
+            if(num1 > 0){
+                for(int k = 0; k <= num1; k++){
+                    for(int h = 0; h < k; h++){
+                        cout <<"*";
+                    }
+                    cout <<endl;
+                }
+                cout <<"[+] Done" <<endl;
+            }else{
+                cout <<"Error: size can't be <= 0" <<endl;
+                return 0;
+            }
+            break;
+        default:
+        cout <<"Error: Unknown figure type" <<endl;
         break;
     }
 }
