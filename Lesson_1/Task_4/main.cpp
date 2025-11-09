c7d6df4babb663039178d1ccd03b13cc191a66bb
voxen-bite <voxen001@gmail.com>
2025-11-10 00:02:07 +0200

    Refactor figure drawing logic with size validation

--- Lesson_1/Task_4/main.cpp
+++ Lesson_1/Task_4/main.cpp
@@ -5,44 +5,42 @@ using namespace std;
 int main()
 {
     char type;
-    int num1;
-    cout <<"Which figure do you wanna draw? s / t" <<endl;
+    int size;
+
+    cout << "Which figure do you wanna draw? s / t" << endl;
     cin >> type;
-    switch (type){
-        case 's': //square
-            cout <<"What size do you wanna draw?" <<endl;
-            cin >> num1;
-            if (num1 > 0){
-                for(int i = 0; i < num1; i++){
-                    for(int j = 0; j < num1; j++){
-                        cout <<"*";
-                    }
-                    cout <<endl;
-                }
-                cout <<"[-] Done" <<endl;
-            }else{
-                cout <<"Error: size can't be <= 0" <<endl;
-                return 0;
+    cout << "What size do you wanna draw?" << endl;
+    cin >> size;
+    while (size <= 0) {
+        cout << "Enter correct size number: " <<endl;
+        cin >> size;
+    }
+
+    switch (type) {
+    case 's': // square
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                cout << "*";
             }
-            break;
-        case 't': // triangle
-            cout <<"What size do you wanna draw?" <<endl;
-            cin >> num1;
-            if(num1 > 0){
-                for(int k = 0; k <= num1; k++){
-                    for(int h = 0; h < k; h++){
-                        cout <<"*";
-                    }
-                    cout <<endl;
-                }
-                cout <<"[+] Done" <<endl;
-            }else{
-                cout <<"Error: size can't be <= 0" <<endl;
-                return 0;
+            cout << endl;
+        }
+        break;
+
+    case 't': // triangle
+        for (int k = 0; k <= size; k++) {
+            for (int h = 0; h < k; h++) {
+                cout << "*";
             }
-            break;
-        default:
-        cout <<"Error: Unknown figure type" <<endl;
+            cout << endl;
+        }
+        break;
+
+    default:
+        cout << "Error: Unknown figure type" << endl;
         break;
     }
+
+    cout << "[+] Done" << endl;
+
+    return 0;
 }
